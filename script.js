8b3f5d252c994b3b77afa93c00860599273d68a5
Elias Raffaelli <eliasraffaelli@gmail.com>
2021-05-27 14:06:05 -0300

    change size button funciona

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const container = document.querySelector('#grid-container');
 const clearButton = document.querySelector('#clear-button');
+const changeButton = document.querySelector('#change-button');
+let size=16;
 
 clearButton.addEventListener("click", resetGrid);
+changeButton.addEventListener("click", changeSize);
+window.addEventListener("load", setGrid);
 
-let i, j;
-for (i=0; i<16; i++){
-    for (j=0; j<16; j++){
-        let grd = document.createElement('div');
-        grd.classList.add('gridItem');
-        grd.setAttribute('onmouseover', 'mouseOver(this)');
-        container.appendChild(grd);
+function setGrid() {
+    let i, j;
+    for (i=0; i<size; i++){
+        for (j=0; j<size; j++){
+            let grd = document.createElement('div');
+            grd.classList.add('gridItem');
+            grd.setAttribute('onmouseover', 'mouseOver(this)');
+            container.appendChild(grd);
+        }
     }
 }
 
@@ -31,4 +37,23 @@ function mouseOver(aItem){
 function resetGrid() {
     let allItems = container.childNodes; 
     allItems.forEach(item => (item.style.backgroundColor = "white"));
+}
+
+function changeSize() {
+    let newSize = prompt('Enter a new size between 1 and 80! :)');
+    if (newSize !== null){
+        if ((isNaN(newSize)) || (newSize < 1 || newSize > 80)) {
+            alert('The number has to be between 0 and 80!');
+            changeSize();
+        } else {
+            size = parseInt(newSize);
+            clearGrid();
+            container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+            setGrid();
+        }
+    }
+}
+
+function clearGrid(){
+    Array.from(container.childNodes).forEach(item => container.removeChild(item))
 }
